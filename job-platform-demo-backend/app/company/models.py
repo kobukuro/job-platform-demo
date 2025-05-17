56cf139bb902ddb0cfdb1acde44e3693b8a524ee
Peter Chen <sanpeter992000@gmail.com>
2025-05-17 19:21:44 +0800

    Alter Company name field to be unique.

--- job-platform-demo-backend/app/company/models.py
+++ job-platform-demo-backend/app/company/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
